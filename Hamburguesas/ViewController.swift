a1b4703d67ef63d1e961d82ad1cb0b0fcf5b2ff6
Leidy Johanna Carvajal Ortiz <iMuffins@macbook-pro-de-leidy.local>
2016-06-11 17:54:07 -0500

    Initial Commit

---
+++ Hamburguesas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hamburguesas
+//
+//  Created by Leidy Johanna Carvajal Ortiz on 11/06/16.
+//  Copyright © 2016 Leidy Johanna Carvajal Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
